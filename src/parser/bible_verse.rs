9ae086473789662590dc782f49059cf59c0fb552
Elsie <elsie19@pm.me>
2024-03-15 12:45:30 -0500

    Add unit tests for bible verse parser

--- src/parser/bible_verse.rs
+++ src/parser/bible_verse.rs
@@ -23,13 +23,13 @@ pub fn range_to_rs_range(range: &str) -> (usize, usize) {
     )
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum BibleRange {
     Number(usize),
     Range((usize, usize)),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct ReturnedBibleVerse {
     pub book: String,
     pub section: Option<String>,
@@ -56,7 +56,7 @@ pub fn parse_verse(verse: &String) -> ReturnedBibleVerse {
                     Rule::range => Some(BibleRange::Range(range_to_rs_range(
                         line.into_inner().next().unwrap().as_str().trim(),
                     ))),
-                    Rule::number => Some(BibleRange::Number(
+                    Rule::verse_number => Some(BibleRange::Number(
                         line.into_inner()
                             .next()
                             .unwrap()
@@ -76,5 +76,107 @@ pub fn parse_verse(verse: &String) -> ReturnedBibleVerse {
         book,
         section,
         verse: opt_bible_verse_range,
+    }
+}
+
+// Should conform to https://developers.sefaria.org/docs/text-references
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_name() {
+        assert_eq!(
+            parse_verse(&"Genesis".to_string()),
+            ReturnedBibleVerse {
+                book: "Genesis".to_string(),
+                section: None,
+                verse: None,
+            }
+        );
+    }
+
+    #[test]
+    fn comma_name() {
+        assert_eq!(
+            parse_verse(&"Zohar, Noach".to_string()),
+            ReturnedBibleVerse {
+                book: "Zohar, Noach".to_string(),
+                section: None,
+                verse: None,
+            }
+        );
+    }
+
+    #[test]
+    fn simple_name_with_section() {
+        assert_eq!(
+            parse_verse(&"Genesis 1".to_string()),
+            ReturnedBibleVerse {
+                book: "Genesis".to_string(),
+                section: Some(1.to_string()),
+                verse: None,
+            }
+        );
+    }
+
+    #[test]
+    fn simple_name_with_complex_section() {
+        assert_eq!(
+            parse_verse(&"Deuteronomy 21b".to_string()),
+            ReturnedBibleVerse {
+                book: "Deuteronomy".to_string(),
+                section: Some("21b".to_string()),
+                verse: None,
+            }
+        );
+    }
+
+    #[test]
+    fn complex_name_with_complex_section() {
+        assert_eq!(
+            parse_verse(&"Zohar, Bo 21b".to_string()),
+            ReturnedBibleVerse {
+                book: "Zohar, Bo".to_string(),
+                section: Some("21b".to_string()),
+                verse: None,
+            }
+        );
+    }
+
+    #[test]
+    fn simple_name_with_verse() {
+        assert_eq!(
+            parse_verse(&"Exodus 1:2".to_string()),
+            ReturnedBibleVerse {
+                book: "Exodus".to_string(),
+                section: Some("1".to_string()),
+                verse: Some(BibleRange::Number(2)),
+            }
+        );
+    }
+
+    #[test]
+    fn simple_name_with_range() {
+        assert_eq!(
+            parse_verse(&"Leviticus 22:2-10".to_string()),
+            ReturnedBibleVerse {
+                book: "Leviticus".to_string(),
+                section: Some("22".to_string()),
+                verse: Some(BibleRange::Range((2, 10))),
+            }
+        );
+    }
+
+    #[test]
+    fn most_complex_everything() {
+        assert_eq!(
+            parse_verse(&"4 Imaginary, Book 7b:2-10".to_string()),
+            ReturnedBibleVerse {
+                book: "4 Imaginary, Book".to_string(),
+                section: Some("7b".to_string()),
+                verse: Some(BibleRange::Range((2, 10))),
+            }
+        );
     }
 }
