4a4a70dff9176a91041cd8cea22155fc0867e576
Elsie <elsie19@pm.me>
2024-03-16 15:15:56 -0500

    Spoofify it

--- src/parser/info.rs
+++ src/parser/info.rs
@@ -6,7 +6,7 @@ pub fn handle_info(index: &Shape, _book: &str) -> Result<String, anyhow::Error> 
     let mut info_vec: Vec<String> = vec![];
     for section in index {
         info_vec.push(format!(
-            "**{}** ~ **{}**\nChapters: **{}**\nVerses: **{}**",
+            "## **{}** ~ **{}**\n> Chapters: **{}**\n> Verses: **{}**",
             section.title,
             section.section,
             section.length,
@@ -18,6 +18,6 @@ pub fn handle_info(index: &Shape, _book: &str) -> Result<String, anyhow::Error> 
             "Could not loop over index in 'handle_info' in subcommand 'info'"
         ))
     } else {
-        Ok(info_vec.join("\n").to_string())
+        Ok(info_vec.join("\n---\n").to_string())
     }
 }
