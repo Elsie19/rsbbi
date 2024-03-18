f1065dcc6964e9297adcf9583f2c88b3a1c03c31
Elsie <elsie19@pm.me>
2024-03-17 20:51:23 -0500

    Whatever

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,7 @@ fn main() {
             }
 
             match bible_verse_range {
-                BibleRange::Range((first, _last)) => {
+                BibleRange::Range((first, _)) | BibleRange::ChapterRange((_, first), (_, _)) => {
                     for (idx, _line) in output_vec.iter().enumerate() {
                         formatted_string.push(if *lines {
                             format!("> *{}* {}", (idx + first), output_vec.get(idx).unwrap())
@@ -113,62 +113,6 @@ fn main() {
                         ));
                     } else {
                         formatted_string.push(format!("> {}\n", output_vec.first().unwrap()));
-                    }
-                }
-                //NOTE: Hey because I'm a dumb fucking idiot this only works when you have
-                //contiguous chapters, so fuck me I guess
-                BibleRange::ChapterRange((_, first_verse), (_, second_verse)) => {
-                    let mut seep_into_chapter = false;
-                    for (idx, _) in output_vec.iter().enumerate() {
-                        if idx != output_vec.len() - second_verse {
-                            let verse_offsetter = if seep_into_chapter {
-                                idx as i64 - output_vec.len() as i64 + second_verse as i64 + 1
-                            } else {
-                                (idx + first_verse).try_into().unwrap()
-                            };
-                            formatted_string.push(if *lines {
-                                format!(
-                                    "> *{}* {}",
-                                    (verse_offsetter),
-                                    output_vec.get(idx).unwrap()
-                                )
-                            } else {
-                                format!("> {}", output_vec.get(idx).unwrap())
-                            });
-
-                            if seep_into_chapter && idx == output_vec.len() - 1 {
-                                formatted_string.push("\n".to_string())
-                            } else {
-                                formatted_string.push(
-                                    if idx != output_vec.len() - second_verse - 1 {
-                                        "\n>\n".to_string()
-                                    } else {
-                                        "\n".to_string()
-                                    },
-                                );
-                            }
-                        } else {
-                            formatted_string.push("---\n".to_string());
-                            formatted_string.push(format!(
-                                "# {}",
-                                parsed_json
-                                    .get("spanningRefs")
-                                    .expect("Could not parse out 'book'")
-                                    .as_array()
-                                    .unwrap()
-                                    .get(1)
-                                    .unwrap()
-                                    .as_str()
-                                    .unwrap(),
-                            ));
-                            formatted_string.push("\n---\n".to_string());
-                            formatted_string.push(if *lines {
-                                format!("> *{}* {}\n>\n", 1, output_vec.get(idx).unwrap())
-                            } else {
-                                format!("> {}\n>\n", output_vec.get(idx).unwrap())
-                            });
-                            seep_into_chapter = true;
-                        }
                     }
                 }
             }
